33824f12f4827ccf8dc666a484af449378f9f885
GiovanniTorrisi28 <TRRGNN02A28C351N@studium.unict.it>
2026-01-13 19:45:06 +0100

    Aggiunto url di connessione al file application.properties

--- src/main/java/com/project/EnergyMarket/Web3jConfig.java
+++ src/main/java/com/project/EnergyMarket/Web3jConfig.java
@@ -1,5 +1,6 @@
 package com.project.EnergyMarket;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.web3j.protocol.Web3j;
@@ -7,9 +8,12 @@ import org.web3j.protocol.http.HttpService;
 
 @Configuration
 public class Web3jConfig {
+    
+    @Value("${blockchain.rpc.url}")
+    private String rpcUrl;
 
     @Bean
     public Web3j web3j() {
-        return Web3j.build(new HttpService("http://localhost:8545"));
+        return Web3j.build(new HttpService(rpcUrl));
     }
 }
